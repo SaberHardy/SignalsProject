d8abc00d69aa716c8d762c7cff61178510efd5e3
SaberHardy <samirsamado5@gmail.com>
2022-09-29 19:30:43 +0100

    pre save signal method

--- signalsApp/models.py
+++ signalsApp/models.py
@@ -5,19 +5,44 @@ from django.utils.text import slugify
 
 from django.dispatch import receiver
 from django.db.models.signals import (
+    pre_save,  # does not have a created variable
     post_save,
 )
 
 User = settings.AUTH_USER_MODEL
 
 
+@receiver(pre_save, sender=User)
+def user_pre_save_receiver(sender, instance, *args, **kwargs):
+    """
+    before saved into database
+    :param sender:
+    :param instance:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    print(f"Send email to {instance.username} - {instance.id}")
+
+
 @receiver(post_save, sender=User)
-def user_created_handler(sender, instance, created, *args, **kwargs):
+def user_post_save_receiver(sender, instance, created, *args, **kwargs):
+    """
+    After saved into database
+    :param sender:
+    :param instance:
+    :param created:
+    :param args:
+    :param kwargs:
+    :return:
+    """
     if created:
+        instance.save()
         print(f"Send email to {instance.username}")
+        # We need to add a saved method to save the instance
     else:
-        print(f"The user has been saved-- {instance.username}")
+        print(f"The user has been saved-- {instance.username} ==> {instance.id}")
 
 
 # receiver function, sender
-# post_save.connect(user_created_handler, sender=User)
+pre_save.connect(user_pre_save_receiver, sender=User)
