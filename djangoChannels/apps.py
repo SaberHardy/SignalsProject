e811cc7c6dbff72915eece5bbb0be8affb48539a
SaberHardy <samirsamado5@gmail.com>
2022-10-11 21:40:12 +0100

    channels app

---
+++ djangoChannels/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangochannelsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djangoChannels'
---
+++ djangoChannels/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def channel(request):
+    return render()
